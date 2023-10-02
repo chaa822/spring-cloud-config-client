88e771652a1926d7f9f7fe2978f98e99eb0242f1
chaa822 <chaa822@naver.com>
2023-10-02 23:17:18 +0900

    Initial commit

---
+++ src/test/java/org/spring/cloud/config/client/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.cloud.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
